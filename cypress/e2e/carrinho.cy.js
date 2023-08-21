327c505128527c23476129671c47f970caf5fee0
Maria Helena Gomes <114106107+MhelenaGomes@users.noreply.github.com>
2023-08-21 02:03:28 -0300

    Update carrinho.cy.js

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -16,8 +16,8 @@ describe('carrinho', () => {
         cy.get('#shopping_cart_container').should('have.text', '1')
     })
 
-    //Este caso de teste consiste em verificar se 2 produto que foi adicionado ao carrinho foi removido 1 produto do carrinho, restando assim o total de 1 produtos.
-    // Tambem consiste em verificar o contador de itens do icone carrinho apos remover produto.
+    //Este caso de teste tem como objetivo verificar se, após a remoção de um produto do carrinho de compras que originalmente continha 2 produtos,
+    //resta apenas 1 produto no carrinho. Além disso, será verificado se o contador de itens no ícone do carrinho é atualizado corretamente após a remoção do produto.
     it('remover produto', () => {
         cy.contains('ADD TO CART').click();
         cy.contains('ADD TO CART').click();
